04106c7f2526eed391e031b1d068c87dddbf7809
Mauricio Zambrano-Bigiarini <hzambran@users.noreply.github.com>
2013-05-07 23:01:31 +0000

    updated CITATION file(s)

--- R/zl.R
+++ R/zl.R
@@ -1,12 +1,12 @@
 # File zFirst.R
 # Part of the hydroPSO R package, http://www.rforge.net/hydroPSO/ ; 
 #                                 http://cran.r-project.org/web/packages/hydroPSO
-# Copyright 2011-2012 Mauricio Zambrano-Bigiarini & Rodrigo Rojas
+# Copyright 2011-2013 Mauricio Zambrano-Bigiarini & Rodrigo Rojas
 # Distributed under GPL 2 or later
 
 .onAttach <- function(libname, pkgname) {
 
-  packageStartupMessage("(C) 2011-2012 M. Zambrano-Bigiarini and R. Rojas (GPL >=2 license)\n",
+  packageStartupMessage("(C) 2011-2013 M. Zambrano-Bigiarini and R. Rojas (GPL >=2 license)\n",
                          "Type 'citation('hydroPSO')' to see how to cite this package")
   invisible()
     
