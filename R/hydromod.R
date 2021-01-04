a5a8373bfd17a3d16f0b8c1f6bb19dd584420f84
r-marinao <ra.marinao.rivas@gmail.com>
2021-01-04 11:17:22 -0300

    Update hydromod.R
    A default character was added in the new argument 'exe.args'

--- R/hydromod.R
+++ R/hydromod.R
@@ -33,7 +33,7 @@ hydromod <- function(
                      param.files="ParamFiles.txt", # Character, with the name of the file (with full path) that stores the name of the files that have to be modified for each parameter 
                      model.drty=getwd(),           # Character, with the path of the directory that stores the exe file of the hydrological model and ALL the input files required for the simulation
                      exe.fname,                    # Character, with the file name of the external executable
-                     exe.args,                     # Character, with optional command line arguments to be entered through a prompted string to execute the user-defined model
+                     exe.args = character(),       # Character, with optional command line arguments to be entered through a prompted string to execute the user-defined model
                      stdout=FALSE,                 # a logical (not NA) indicating whether messages written to 'stdout' should be sent or not. See '?system2'
                      stderr="",                    # a logical (not NA) indicating whether messages written to 'stderr' should be sent or not. See '?system2'
                      verbose= FALSE,               # logical, indicating if progress messages have to be printed during the simulations. If \code{verbose=TRUE}, the following messages will appear: i)parameter values for each particle; (ii) model execution; iii) extraction of simulated values; and iv) computation of the goodness-of-fit measure.
